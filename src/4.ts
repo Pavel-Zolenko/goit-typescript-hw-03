7466e92cedcffc8433d5a6ece9776a92917bda4a
Pavel-Zolenko <107986786+Pavel-Zolenko@users.noreply.github.com>
2023-09-09 14:09:47 +0300

    hw3

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,66 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    // Генеруємо випадкову сигнатуру за допомогою Math.random()
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Welcome home!");
+    } else {
+      console.log("The door is closed. You can't come in.");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is open.");
+    } else {
+      console.log("The key does not match. The door remains closed.");
+    }
+  }
+}
+
 const key = new Key();
 
-const house = new MyHouse(key);
 const person = new Person(key);
+const house = new MyHouse(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
